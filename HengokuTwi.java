57dff26c84ddce982f432cca618027ef61adaad8
Toshihiko Asahi <lovingcat222@gmail.com>
2015-07-14 23:19:17 +0900

    Twitterに投げる文言を模索中

--- HengokuTwi.java
+++ HengokuTwi.java
@@ -3,20 +3,51 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.util.Scanner;
 public class HengokuTwi {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
+		TweetManager tm = TweetManager.getInstance();
+		if(!tm.isAuthorized()){
+			System.out.println(tm.createRequestToken());
+			Scanner s = new Scanner(System.in);
+			String pin = s.nextLine();
+			s.close();
+			tm.authorize(pin);
+		}
+		
+		//ツイート内容の決定(外部ファイルからフィルタリング設定等読み込む)
+		//Default.dbへのパスも一応読み込むことにする
+		//Scanner s = new Scanner("config.txt");
+		//s.close();
+		
+		StringBuilder sb = new StringBuilder();
 		RecordGetter getter = RecordGetter.createInstance("Default.db");
+		//共通で総合戦績は出す。あとメインキャラも
 		RecordInfo info = getter.getRecordInfo();
-		System.out.println("総試合数:" + info.getRecordCount());
-		System.out.println("勝ち:" + info.getWins());
-		System.out.println("勝率:" + info.getWinningPercentage());
-
-		//filterのテスト
-		RecordFilter filter = new RecordFilter(20,Character.UNSPECIFIED, Character.MARISA);
-		info = getter.getRecordInfo(filter);
-		System.out.println("総試合数:" + info.getRecordCount());
-		System.out.println("勝ち:" + info.getWins());
-		System.out.println("勝率:" + info.getWinningPercentage());
+		Character mainChara = getter.getMainCharacter();
+		sb.append("＊あなたの東方深秘録戦績＊").append('\n');
+		//以下は設定ファイルで規定する↓
+		//メインキャラ
+		sb.append("最もよく使うキャラ:" + mainChara).append('\n');
+		//総合
+		sb.append(info.toString()).append('\n');
+		//直近50
+		info = getter.getRecordInfo(new RecordFilter(50));
+		sb.append(info.toString()).append('\n');		
+		//直近30
+		info = getter.getRecordInfo(new RecordFilter(30));
+		sb.append(info.toString()).append('\n');
+		//魔理沙戦での戦績
+		info = getter.getRecordInfo(new RecordFilter(Character.UNSPECIFIED, Character.MARISA));
+		sb.append(info.toString()).append('\n');
+		//直近40戦の対こいし戦の戦績
+		info = getter.getRecordInfo(new RecordFilter(40, Character.UNSPECIFIED, Character.KOISHI));
+		sb.append(info.toString()).append('\n');
+		sb.append("#東方深秘録");
+		
+		System.out.println(sb.toString());
+		System.out.println(sb.length());
+		//tm.tweet(sb.toString());
+		//カスタムで設定ファイルによって規定される内容をツイートする
 	}
 }
