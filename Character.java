b97af66362a4e0e338f3b402d5a662a5ef13246d
Toshihiko Asahi <lovingcat222@gmail.com>
2015-07-16 02:23:29 +0900

    キャラ名短縮

--- Character.java
+++ Character.java
@@ -21,13 +21,21 @@ public enum Character{
 			return Character.UNSPECIFIED;
 		return enumValues[id];
 	}
-	private String[] names = {
+	/*private final static String[] names = {
 		"博麗霊夢", "霧雨魔理沙",
 		"雲居一輪＆雲山","聖白蓮",
 		"物部布都", "豊郷耳神子",
 		"河城にとり", "古明地こいし", "二ツ岩マミゾウ", "秦こころ",
 		"茨木華仙", "藤原妹紅", "少名針妙丸", "宇佐見董子"
+	};*/
+	private final static String[] names = {
+		"霊夢", "魔理沙",
+		"一輪","聖",
+		"布都", "神子",
+		"にとり", "こいし", "マミゾウ", "こころ",
+		"華仙", "妹紅", "針妙丸", "董子"
 	};
+
 	private static Character[] enumValues = {
 		Character.REIMU, Character.MARISA,
 		Character.ICHIRIN,Character.BYAKUREN,
