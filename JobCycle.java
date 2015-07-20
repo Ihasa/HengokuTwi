432e56c01b159e73810c6800598edadaf45bb60f
Toshihiko Asahi <lovingcat222@gmail.com>
2015-07-21 01:28:34 +0900

    定期ポスト開始時刻を設定可能に

--- JobCycle.java
+++ JobCycle.java
@@ -1,56 +1,25 @@
 import java.time.LocalDateTime;
+import java.time.Duration;
 
-public abstract class JobCycle{
-	public abstract boolean isTime(LocalDateTime now);
-}
+public class JobCycle{
+	private LocalDateTime start;
+	private Duration duration;
 
-class EveryDay extends JobCycle{
-	int day, hour, min, second;
-	public EveryDay(int day, int hour, int min, int second){
-		this.day = day;
-		this.hour = hour;
-		this.min = min;
-		this.second = second;
+	public JobCycle(LocalDateTime start, Duration duration){
+		this.start = start;
+		this.duration = duration;
 	}
-	public boolean isTime(LocalDateTime now){
-		return now.getDayOfMonth() % day == 0 && now.getHour() == hour && now.getMinute() == min && now.getSecond() == second;
+	public JobCycle(Duration d){
+		this(LocalDateTime.now(),d);
 	}
-}
 
-class EveryHour extends JobCycle{
-	int hour, min, second;
-	public EveryHour(int hour, int min, int second){
-		this.hour = hour;
-		this.min = min;
-		this.second = second;
+	public LocalDateTime getStartDateTime(){
+		return start;
 	}
-	public boolean isTime(LocalDateTime now){
-		return (now.getHour() % hour == 0 && now.getMinute() == min && now.getSecond() == second);
+	public Duration getDuration(){
+		return duration;
 	}
-}
-
-
-class EveryMin extends JobCycle{
-	int second;
-	int min;
-	public EveryMin(int min, int second){
-		this.min = min;
-		this.second = second;
-	}
-	public boolean isTime(LocalDateTime now){
-		return (now.getMinute() % min == 0 && now.getSecond() == second);
-	}
-}
-
-class EverySecond extends JobCycle{
-	int second;
-	public EverySecond(){
-		this(1);
-	}
-	public EverySecond(int s){
-		second = s;
-	}
-	public boolean isTime(LocalDateTime now){
-		return now.getSecond() % second == 0;
+	public boolean isActive(){
+		return start.compareTo(LocalDateTime.now()) <= 0;
 	}
 }
--- JobThread.java
+++ JobThread.java
@@ -3,18 +3,20 @@ import java.time.Duration;
 
 public class JobThread extends Thread{
 	int sleepSec;
-	Duration duration;
 	Job job;
+	JobCycle cycle;
 	public JobThread(Job j, int cycle){
 		job = j;
 		sleepSec = cycle;
-		duration = Duration.ofSeconds(4);
+		this.cycle = new JobCycle(Duration.ofSeconds(4));
 	}
 	public void run(){
 		while(true){
 			try{
-				Thread.sleep(duration.toMillis());
-				job.execute();
+				if(cycle.isActive()){
+					Thread.sleep(cycle.getDuration().toMillis());
+					job.execute();
+				}
 			}catch(Exception e){
 				System.out.println(e);
 			}
