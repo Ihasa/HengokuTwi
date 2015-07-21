a85392dcf3e3718e1fb53a7d991b71e256d036ec
Toshihiko Asahi <lovingcat222@gmail.com>
2015-07-21 14:55:46 +0900

    定期ポストに対応|文字数オーバーしないように修正

--- HengokuTwi.java
+++ HengokuTwi.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
 public class HengokuTwi {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		TweetManager tm = TweetManager.getInstance();
 		if(!tm.isAuthorized()){
 			System.out.println(tm.createRequestToken());
@@ -16,7 +16,11 @@ public class HengokuTwi {
 			s.close();
 			tm.authorize(pin);
 		}
-
+		JobThread thread = new JobThread(()->post(tm));
+		thread.start();
+		thread.join();
+		//post();
+		/*
 		//ツイート内容の決定(外部ファイルからフィルタリング設定等読み込む)
 		//Default.dbへのパスも一応読み込むことにする
 		//List<RecordFilter> filters = getFiltersFromFile();
@@ -51,15 +55,58 @@ public class HengokuTwi {
 		
 		System.out.println(sb.toString());
 		System.out.println(sb.length());
-		
-		JobThread p = new JobThread(
-			() -> {System.out.println("YEY");},
-			3
-		);
-		p.start();
-		p.join();
+		*/
 		/*
 		if(tm.tweet(sb.toString()) != null)
 			System.out.println("tweeted");*/
+	}
+	private static void post(TweetManager tm){
+		try{
+			//ツイート内容の決定(外部ファイルからフィルタリング設定等読み込む)
+			//Default.dbへのパスも一応読み込むことにする
+			//List<RecordFilter> filters = getFiltersFromFile();
+			ConfigReader cr = new ConfigReader("config.txt");
+			List<RecordFilter> filters = cr.getFilters();
+
+			String title = "＊あなたの東方深秘録戦績＊";
+			String hashes = "#東方深秘録　#辺獄録　#HengokuTwi";
+
+			StringBuilder sb = new StringBuilder();
+			sb.append(title).append("\n\n");
+			RecordGetter getter = RecordGetter.createInstance("Default.db");
+			//フィルターに基づいてツイート
+			if(filters.size() > 0){
+				for(RecordFilter f : filters){
+					RecordInfo info = getter.getRecordInfo(f);
+					if(info != null){
+						String s = info.toString() + "\n\n";
+						if(sb.length() + hashes.length() + s.length() <= 140)
+							sb.append(s);
+					}
+				}
+			}else{
+				//総合
+				RecordInfo info = getter.getRecordInfo();
+				sb.append(info.toString()).append("\n\n");
+				//直近100
+				info = getter.getRecordInfo(100);
+				sb.append(info.toString()).append("\n\n");
+				//直近50
+				info = getter.getRecordInfo(50);
+				sb.append(info.toString()).append("\n\n");
+				//直近30
+				info = getter.getRecordInfo(30);
+				sb.append(info.toString()).append("\n\n");
+			}
+			sb.append(hashes);
+			System.out.println(sb.toString());
+			System.out.println(sb.length());
+
+			/*
+			if(tm.tweet(sb.toString()) != null)
+				System.out.println("tweeted");*/
+		}catch(Exception e){
+			System.out.println(e);
+		}
 	}
 }
